57fa8a640e7755fc8d7657110c296d6280be2580
sgcs11 <joachy@naver.com>
2022-07-10 17:37:11 +0900

    BaseTimeEntity abstract 추가

--- src/main/java/tripong/backend/entity/BaseTimeEntity.java
+++ src/main/java/tripong/backend/entity/BaseTimeEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -12,8 +13,9 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
