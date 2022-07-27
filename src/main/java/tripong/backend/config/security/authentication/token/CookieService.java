802344f7cba85fe62d4aab8edb15348ce846a330
lena1005a <lena1005a@gmail.com>
2022-07-27 21:07:52 +0900

    테스트 환경: 쿠키 도메인 수정(127.0.0.1)

--- src/main/java/tripong/backend/config/security/authentication/token/CookieService.java
+++ src/main/java/tripong/backend/config/security/authentication/token/CookieService.java
@@ -16,11 +16,11 @@ public class CookieService {
     public String refreshCookieIn(String refreshToken){
         log.info("시작: CookieService - 쿠키생성");
         ResponseCookie refreshCookie = ResponseCookie.from(RefreshTokenProperties.HEADER_STRING, refreshToken)
-//                .domain("localhost")
+                .domain("127.0.0.1")
 //                .domain("tripong-development.herokuapp.com")
                 .maxAge(RefreshTokenProperties.EXPIRATION_TIME)
-                .secure(true)
                 .sameSite("None")
+                .secure(true)
                 .httpOnly(true)
                 .path("/")
                 .build();
