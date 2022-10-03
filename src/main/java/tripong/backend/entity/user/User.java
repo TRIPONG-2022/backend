b57b81f57d4a0ab740495ae6244324a2f6f617b9
sgcs11 <joachy@naver.com>
2022-10-03 21:36:17 +0900

    태그 코드 수정

--- src/main/java/tripong/backend/entity/user/User.java
+++ src/main/java/tripong/backend/entity/user/User.java
@@ -63,7 +63,7 @@ public class User extends BaseTimeEntity{
 
     private BigDecimal longitude;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Tag> tags = new ArrayList<>();
 
     ////-----편의 메소드-----
